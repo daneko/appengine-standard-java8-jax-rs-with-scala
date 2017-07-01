69b31c6d9b4102aa5ca1cd4b4907b78c4af5970e
daneko <3a3k.web+github@gmail.com>
2017-07-01 23:32:47 +0900

    ad json sample

---
+++ app/src/main/scala/com/example/sample/HelloJsonResource.scala
@@ -0,0 +1,52 @@
+package com.example.sample
+
+import javax.ws.rs._
+import javax.ws.rs.core.MediaType
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+/**
+  * https://www.mkyong.com/webservices/jax-rs/jax-rs-queryparam-example/
+  */
+@Path("hello_json")
+class HelloJsonResource {
+
+  /*
+   * curl http://localhost:8080/hello_json/hoge?q=hoge
+   */
+  @GET
+  @Path("{path}")
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  def json(@PathParam("path") path: String,
+           @DefaultValue("default") @QueryParam("q") query: String): HelloJsonResponseBody = {
+    HelloJsonResponseBody(path, query)
+  }
+
+  /*
+   * curl -X POST -d '{"a":1, "b":"hoge"}' -H 'Content-Type: application/json' http://localhost:8080/hello_json
+   *
+   * { "a" : 1 } だと bはnull扱い
+   * { "a" : 1, "c" :"hoge"} だと
+   * Unrecognized field "c" (class com.example.sample.RequestBody), not marked as ignorable (2 known properties: "a", "b"])
+   *  at [Source: org.glassfish.jersey.message.internal.ReaderInterceptorExecutor$UnCloseableInputStream@155a1984; line: 1, column: 20] (through reference chain: com.example.sample.RequestBody["c"])%
+   *
+   */
+  @POST
+  @Consumes(Array(MediaType.APPLICATION_JSON))
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  def echo(requestBody: RequestBody): RequestBody = requestBody
+
+}
+
+/*
+ * Can not construct instance of com.example.sample.HelloJsonResource$RequestBody: can only instantiate non-static inner class by using default, no-argument constructor
+ *  at [Source: org.glassfish.jersey.message.internal.ReaderInterceptorExecutor$UnCloseableInputStream@ee62f0a; line: 1, column: 2]
+ *  とでるのでここにおいている
+ */
+case class RequestBody(a: Int, b: String)
+
+/*
+ * JsonIgnorePropertiesで未知のPropertiesが無視できない…
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+case class HelloJsonResponseBody(path: String, query: String)
---
+++ app/src/main/scala/com/example/sample/JsonContextResolver.scala
@@ -0,0 +1,33 @@
+package com.example.sample
+
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.ext.{ContextResolver, Provider}
+import javax.ws.rs.{Consumes, Produces}
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+
+/**
+  * [2014年のBlog記事を参考](http://d.hatena.ne.jp/Kazuhira/20141101/1414838251)にしたものの、
+  * ContextResolverが何者で、どうしてObjectMapperを返すと動くのか把握してない。
+  *
+  * [この辺を見てなるほど](https://stackoverflow.com/questions/32478159/jax-rs-contextresolvert-undestanding) と思ったりもするけど
+  * ObjectMapperが選択されるのはまあわかったのだが、なぜObjectMapperを使えるのかがよーわからん。
+  *
+  * 例えばplay-jsonなんかのReads/Writesなんかを使うように書く方法とかもあるんだろうか…
+  */
+@Provider
+@Consumes(Array(MediaType.APPLICATION_JSON))
+@Produces(Array(MediaType.APPLICATION_JSON))
+class JsonContextResolver extends ContextResolver[ObjectMapper] {
+
+  private val mapper = {
+    val _mapper = new ObjectMapper()
+    _mapper.registerModule(DefaultScalaModule)
+    _mapper
+  }
+
+  override def getContext(`type`: Class[_]): ObjectMapper = {
+    mapper
+  }
+}
